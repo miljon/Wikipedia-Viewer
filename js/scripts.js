4cd4e519742e116cf8927ae08caccf6048f5dc5d
miljon <mwmurawski@gmail.com>
2018-05-14 01:38:44 +0200

    Finished project, implemented both buttons and input text

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,37 @@
+const wikiSearch = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+
 $(document).ready(function () {
 
+    $('#wikipediaSearch').keypress(function (e) {
+        var key = e.which;
+        if(key == 13 )  // the enter key code
+         {
+            $('#wikipediaSearchButton').click();
+           return false;  
+         }
+       });   
 
     $('#wikipediaSearchButton').click(function () {
-
+        $.ajax({
+            dataType: "jsonp",
+            type: 'GET',
+            url: wikiSearch + $('#wikipediaSearch').val(),
+            data: null,
+            success: function (data) {
+                console.log(data);
+                var par = '';
+                for (var i = 0; i < data[1].length; i++) {
+                    if (data[1][i] && data[2][i]) {
+                    par += `<p class="search-row">${i}. <b>${data[1][i]}</b> - ${data[2][i]}<p><br>`
+                    }
+                }
+                $('.search-data').html(par);
+            }
+        });
     });
 
     $('#wikipediaRandomButton').click(function () {
-
+        var win = window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');
+        win.focus();
     });
 });
